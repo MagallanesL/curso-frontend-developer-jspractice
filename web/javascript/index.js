c5f88b9c3c2a5bfa1dcc12bedc7b2adcb74b38af
Luis <alu.magallanes@gmail.com>
2022-07-24 23:47:02 -0300

    asideclose

--- web/javascript/index.js
+++ web/javascript/index.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuBurger = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const shopCar = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurger.addEventListener("click", toggleMobileMenu);
+shopCar.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
@@ -12,4 +15,12 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
   mobileMenu.classList.toggle("inactive");
+}
+function toggleCarAside() {
+
+const inMobileMenuClosed =mobileMenu.classList.contains('inactive')
+const isAsideClosed =aside.classList.contains('inactive')
+
+  aside.classList.toggle("inactive");
+  if(mobileMenu.classList.contains('inactive'))
 }
