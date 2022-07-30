2b55214a3d65d65d41282ac47f3a4c893462dd86
Luis <you@example.com>
2022-07-30 20:07:32 -0300

    .

--- web/javascript/index.js
+++ web/javascript/index.js
@@ -4,89 +4,74 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
-
+const cardsContainer = document.querySelector(".cards-container");
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
-
 function toggleDesktopMenu() {
   const isAsideClosed = aside.classList.contains("inactive");
-
   if (!isAsideClosed) {
     aside.classList.add("inactive");
   }
-
   desktopMenu.classList.toggle("inactive");
 }
-
 function toggleMobileMenu() {
   const isAsideClosed = aside.classList.contains("inactive");
-
   if (!isAsideClosed) {
     aside.classList.add("inactive");
   }
-
   mobileMenu.classList.toggle("inactive");
 }
-
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
-
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
-
   aside.classList.toggle("inactive");
 }
-
 const productList = [];
 productList.push({
   name: "Bike",
   price: 120,
-  imagen:
+  image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 productList.push({
-  name: "Teclado",
-  price: 100,
-  imagen:
-    "https://computronik.com.ec/wp-content/uploads/2020/09/teclado-gamer-retroiluminado-D_NQ_NP_783934-MEC42361752801_062020-F-1024x727.jpg",
+  name: "Pantalla",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 productList.push({
-  name: "Teclado Flex",
-  price: 180,
-  imagen:
-    "https://i0.wp.com/movilesbaratos.com.mx/wp-content/uploads/2017/02/teclado_portatil_enrollable.jpg?fit=1200%2C1200&ssl=1",
+  name: "Compu",
+  price: 620,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
-productList.push({
-  name: "Monitor",
-  price: 1200,
-  imagen:
-    "https://www.gacomputers.in/wp-content/uploads/2020/12/acer-23-8-inch-ips-led-with-webcam-monitor-b247y-1.jpg",
-});
-
-for (product of productList) {
-  const producCard = document.createElement("div");
-  producCard.classList.add("product-card");
-
-  const img = document.createElement("img");
-  img.setAttribute("src", product.imagen);
-
-  const producInfo = document.createElement("div");
-  producInfo.classList.add("product-info");
-
-  const producInfoDiv = document.createElement("");
-  const producPrice = document.createElement("p");
-  producPrice.innerText = "$" + product.price;
-  const producName = document.createElement("p");
-  producName.innerText = product.name;
-
-  producInfoDiv.appendChild(producPrice);
-  producInfoDiv.appendChild(producName);
-
-  const producInfoFigure = document.createElement("figure");
-  const producImgCard = document.createElement("img");
-  img.setAttribute("src", "../icons/bt_add_to_cart.svg");
-
-  producInfoFigure.appendChild(producImgCard);
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    const productInfoDiv = document.createElement("div");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+    productInfoFigure.appendChild(productImgCart);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+    cardsContainer.appendChild(productCard);
+  }
 }
+renderProducts(productList);
